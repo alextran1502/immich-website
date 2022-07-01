8613c41770434acc593f3523ed246c9ddd5706f7
Isaac Fidst <fidst@users.noreply.github.com>
2022-07-01 14:09:23 -0400

    Remove the S from Children
    Children is already the plural of child.

--- src/lib/ui-components/dedication.svelte
+++ src/lib/ui-components/dedication.svelte
@@ -1,7 +1,7 @@
 <div
 	class="py-10 border-t bg-slate-900 flex place-content-center place-items-center font-light flex-col text-gray-200 text-center px-10"
 >
-	<p>This project is dedicated to my wife and my childrens.</p>
+	<p>This project is dedicated to my wife and my children.</p>
 
 	<div class="font-mono text-xs mt-10 ">
 		<p>
